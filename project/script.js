b7d402e0b2b41bbe41e2f85927e02b298c1d6b8f
Eduard Nerkararian <mr.nerkararyna99@gmail.com>
2023-03-04 22:03:14 +0200

    add1

---
+++ project/script.js
@@ -0,0 +1,28 @@
+const operator = prompt("Enter ( +, -, *, / ):");
+
+
+const number1 = parseFloat(prompt("Enter number 1: "));
+const number2 = parseFloat(prompt("Enter number 2: "));
+let result;
+
+if (operator === '+') {
+
+    result = number1 + number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+
+else if (operator === '-') {
+    result = number1 - number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+else if (operator === '*') {
+    result = number1 * number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+else if (operator === '/') {
+    result = number1 * number2;
+    alert("Result: " + number1 + operator + number2 + ' = ' +result);
+}
+else {
+    alert("error");
+}
